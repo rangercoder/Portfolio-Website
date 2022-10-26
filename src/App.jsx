9ebc9e656792d50042da02d78ff2fb7a43cdd809
Pushker Prakash <pprakash.1st@gmail.com>
2022-10-27 00:09:38 +0530

    new update

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/about'
+import Experience from './components/Experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/portfolio'
+import Testimonials from './components/testimonials/testimonials'
+import Contact from './components/Contact/contact'
+
+
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />  
+      <About/>
+      <Experience />
+      <Services/>
+      <Portfolio/>
+      <Testimonials/>
+      <Contact/>
+
+
+
+    </>
+  )
+}
+
+export default App
