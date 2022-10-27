19dc5e461ad1c349cf30d03ab78a246fffa83796
Pushker Prakash <pprakash.1st@gmail.com>
2022-10-28 00:11:46 +0530

    27th Oct

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from'../../Assets/me.png'
+
 const header = () => {
   return (
-    <div>header</div>
+  <header>
+    <div className="container header_container">
+      <h5> Hello I'm</h5>
+      <h1>Pushker Prakash</h1>
+      <h5 className="text-light">Machine Learning Enthusiast</h5>
+      <CTA/>
+
+
+      <div className="me">
+       <img src={ME} alt="me" />
+      </div>
+
+    <a href="#Contact" className='scroll_down'>Scroll Down</a>
+    </div>
+  </header>
   )
 }
 
